7a280fa5ca8ff5e00da4f0fd12b6945833130cbf
Sohan Nannra <49798755+snannra@users.noreply.github.com>
2023-06-26 16:27:35 -0700

    commit

--- main.go
+++ main.go
@@ -3,6 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	// ask user for their name
+	fmt.Print("Type in your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Print("Type in your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Print("Type in your email: ")
+	fmt.Scan(&email)
+	fmt.Print("Enter number of tickets you will buy: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets -= userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+	fmt.Printf("There are %v remaining tickets for %v.\n", remainingTickets, conferenceName)
+
 }
