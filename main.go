88b30c13875eac5f93df5255a232d98b75c5599a
Sohan Nannra <49798755+snannra@users.noreply.github.com>
2023-06-26 15:15:57 -0700

    first file

--- main.go
+++ main.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to our conference booking application")
-	fmt.Println("Get your tickets here to attend")
+	fmt.Println("Get your tickets here to attend:")
 }
